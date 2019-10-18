dd477a10d74881b24146122749856ac6a190fd73
Francesca Tassinari <francesc.tassinari10@studio.unibo.it>
2019-10-18 18:03:49 +0200

    Notrh Wall

---
+++ Assets/Script/GyroCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroCamera : MonoBehaviour {
+
+	private Gyroscope gyro;
+	private bool gyroSupported;
+	private Quaternion rotFix;
+
+	[SerializeField]
+	private Transform worldObj;
+	private float startY;
+
+	[SerializeField]
+	//private Transform zoomObj;
+	// Use this for initialization
+	void Start() {
+		gyroSupported = SystemInfo.supportsGyroscope;
+
+		GameObject camParent = new GameObject("camParent");
+		camParent.transform.position = transform.position;
+		transform.parent = camParent.transform;
+
+		if(gyroSupported)
+		{
+			gyro = Input.gyro;
+			gyro.enabled = true;
+
+			camParent.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
+			rotFix = new Quaternion(0, 0, 1, 0);
+		}
+
+	}
+
+// Update is called once per frame
+	void Update () {
+		transform.localRotation = gyro.attitude * rotFix;
+
+
+	}
+}
