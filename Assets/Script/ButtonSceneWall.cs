42144e33acd3d85debcbc5475cf6eba57cb3a7da
Francesca Tassinari <francesc.tassinari10@studio.unibo.it>
2019-10-22 15:14:18 +0200

    Scene Menu and Video

---
+++ Assets/Script/ButtonSceneWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSceneWall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayVideo() {
+        SceneManager.LoadScene(2);
+    }// play game
+
+}
---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* In the Menu class we deal with loading into the AR scene
+ * We also deal with quitting the game
+ */ 
+public class Menu : MonoBehaviour {
+
+    // start a new game
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }// play game
+    
+    // Close the application
+    public void Quit() {
+        Debug.Log("Exiting game");
+        Application.Quit();
+    }// Quit
+}
